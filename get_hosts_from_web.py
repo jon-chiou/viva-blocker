499ea3ee6bbed2b96caae62d301fefd5505bfe43
chiouonthis <jchiou1984@gmail.com>
2019-04-27 17:39:49 -0400

    Added doc string, removed commented code, added authorship info

--- get_hosts_from_web.py
+++ get_hosts_from_web.py
@@ -1,20 +1,14 @@
 #! usr/bin/benv python3
 
+"""This script grabs a list of known obnoxious urls, parses the text for just the urls,
+ then formats them for Chromium extensions as a .js array with wildcards"""
+
 import requests
 import re
 
 url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 
 response = requests.get(url)
-
-'''
-with open("hosts", "w") as out_file:
-    for line in response.text:
-        out_file.write(line)
-
-out_file.close()
-'''
-
 
 url_line_pattern = re.compile(r"^0\.0\.0\.0 .*")
 
@@ -35,3 +29,4 @@ def format_urls(response_text):
 
 format_urls(response.text.splitlines())
 
+__author__ = "Jonathan Chiou"
