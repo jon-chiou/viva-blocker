057ccc15d9b1dde2020fb5331c41b2068b47a424
chiouonthis <jchiou1984@gmail.com>
2019-04-27 17:21:21 -0400

    Removed sed subprocess from Python script and moved parsing and creation of  into the Python script

--- get_hosts_from_web.py
+++ get_hosts_from_web.py
@@ -1,20 +1,37 @@
 #! usr/bin/benv python3
 
 import requests
-import subprocess
+import re
 
 url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 
 response = requests.get(url)
 
+'''
 with open("hosts", "w") as out_file:
     for line in response.text:
         out_file.write(line)
 
 out_file.close()
+'''
 
-print("Start shell script")
-subprocess.call("./convert_hosts.sh")
-print("Shell script finished")
 
+url_line_pattern = re.compile(r"^0\.0\.0\.0 .*")
+
+
+def format_urls(response_text):
+    with open("blocked_domains.js","w") as blacklist_file:
+        blacklist_file.write("var blocked_domains = [\n")
+        for text in response_text:
+            match_url = url_line_pattern.match(text)
+            if match_url is not None:
+                match_string = match_url.group(0)
+                new_url = match_string.split(" ")
+                formatted_url = "\"*://*." + new_url[1] + "/*\",\n"
+                blacklist_file.write(formatted_url)
+        blacklist_file.write("];")
+        blacklist_file.close()
+
+
+format_urls(response.text.splitlines())
 
